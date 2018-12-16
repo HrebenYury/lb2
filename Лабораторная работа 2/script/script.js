d6bb8c390cccb05c6a6c7e3aa44321ec4cede142
Yury Hreben <urikgreben@gmail.com>
2018-12-16 21:04:02 +0300

    last image

---
+++ Лабораторная работа 2/script/script.js
@@ -0,0 +1,12 @@
+
+menu.onclick=function Navbar(){
+    var x=document.getElementById('myNavBar')
+    if(x.className==='navBar'){
+        x.className+=' responsive';
+    }else
+    {
+        x.className='navBar';
+    }
+}
+
+
